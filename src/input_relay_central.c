c55681c6ad23a39e9b83798f839a10df61ba2ed1
tasin <tklzg@naver.com>
2024-06-23 14:35:13 +0900

    Update input_relay_central.c

--- src/input_relay_central.c
+++ src/input_relay_central.c
@@ -118,7 +118,7 @@ K_MSGQ_DEFINE(peripheral_input_event_msgq, sizeof(struct input_event),
 void peripheral_input_event_work_callback(struct k_work *work) {
     struct input_event ev;
     while (k_msgq_get(&peripheral_input_event_msgq, &ev, K_NO_WAIT) == 0) {
-        LOG_DBG("Trigger input change for %d/%d/%d/%d", 
+        //LOG_DBG("Trigger input change for %d/%d/%d/%d", 
                 ev.type, ev.code, ev.value, ev.sync?0:1);
         switch (ev.type) {
         case INPUT_EV_REL:
